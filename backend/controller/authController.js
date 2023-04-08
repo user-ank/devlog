33ce493a0ec961c1253acdc01fef28265cfe4a7b
user-ank <ankush1maji@gmail.com>
2023-04-08 14:12:40 +0530

    Auth updated
    Cookie marked as secure, homePage added in persistant login, other minor changes

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -28,7 +28,7 @@ const createSendToken = async (user, statusCode, res) => {
         expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
         httpOnly: true,
         sameSite: 'None',
-        // secure: true
+        secure: true
     }
 
     // if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
