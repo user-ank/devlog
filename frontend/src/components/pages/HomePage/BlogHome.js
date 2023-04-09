af05ce4943cd8cd8be4634c3ce0951f07c903427
user-ank <ankush1maji@gmail.com>
2023-04-09 19:51:45 +0530

    Frontend/BookmarkIcon added

--- frontend/src/components/pages/HomePage/BlogHome.js
+++ frontend/src/components/pages/HomePage/BlogHome.js
@@ -1,15 +1,40 @@
-import React from 'react'
-import './BlogHome.css'
+import { useState } from 'react'
 import { Link } from 'react-router-dom';
+import { PrivateAPI } from '../../../api';
+import { wentWrongMsg } from '../../Header/Header';
+import { useAuth } from '../../../context/auth';
+import './BlogHome.css'
 
 function BlogHome(prop) {
 	let { blog } = prop;
-	// console.log("im blog")
+	
+
+	const [is_bookmarked, setBookmark] = useState(false);
+	const {user} = useAuth();
 
 	let blogTime = new Date(blog.updatedAt);
 	let curentTime = new Date();
 	let timeDiff = curentTime.getTime() - blogTime.getTime();
 	let time = new Date(timeDiff);
+
+	function handleBookmark(){
+		if(!user)
+		{
+			
+		}
+	}
+	async function bookmark()
+	{
+		try{
+			await PrivateAPI.get("/posts/bookmark/"+ blog.id);			
+		}
+		catch(err)
+		{
+			console.log(err);
+			wentWrongMsg();
+			setBookmark(prev => !prev);
+		}
+	}
 
 	return (
 		<div className='blogContainer'>
@@ -35,20 +60,22 @@ function BlogHome(prop) {
 					<div className='blogBody'>
 						<div className='blogBodyContentDiv'>
 							<div className='blogBodyTitle'>{blog.title}</div>
-							<div className='blogBodyMinRead'>{5} min read</div>
+							<div className='blogBodyMinRead'>{blog.minute_read} min read</div>
 							<div className='blogBodyContent'>{blog.content.substring(0,200)+"..."}</div>
 						</div>
 						<div className='blogBodyImageDiv'> {/*  put image here */}
 							{blog.ContainImage ? <img className='blogBodyImage' src={blog.photo}/> : null}
-						</div>
-
-						
+						</div>					
 					</div>
 				</Link>
 
 			<div className='blogFooter'>
-
+				<img className='bookmarkIcon'
+					src={is_bookmarked ? require("../../img/bookmark.png") : require("../../img/bookmark-empty.png")}
+					onClick={()=>{setBookmark(prev => !prev)}}
+					/>
 			</div>
+			<div className='bookmarkmsg'>Bookmarked !</div>
 		</div>
 	)
 }
